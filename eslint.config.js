b9aa96533349e88114d593ea14b60ddaa190191d
Usman Ali <usmanalishah5040@gmail.com>
2025-09-05 10:10:39 +0500

    Your commit message

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
+// config.js
+
+// ================= ESLint CONFIG =================
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
+export const eslintConfig = defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
@@ -14,10 +17,9 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
@@ -27,3 +29,17 @@ export default defineConfig([
     },
   },
 ])
+
+module.exports = {
+  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
+module.exports = {
+  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
+
