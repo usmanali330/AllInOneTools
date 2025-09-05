b799695477721cc54058e4e4edd5bfef13a50593
Usman Ali <usmanalishah5040@gmail.com>
2025-09-05 09:41:44 +0500

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AllInOneTools from './allinone'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>usmanlai</h2>
+      <AllInOneTools/>
+    </>
+  )
+}
+
+export default App
