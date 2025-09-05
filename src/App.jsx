4a39e76caef66b36ecc915e45b1c892d0f9f3bbb
Usman Ali <usmanalishah5040@gmail.com>
2025-09-05 16:05:49 +0500

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
-import Card from "./servises-card";
-import Form from "./forms";
-import UsmanPortfolio from "./UsmanPortfolio";
-import Weather from "./weather-Cards";
-import WeatherForecast from "./WeatherForecast";
-import Game from "./Tic-Tac-Toe"; 
-import TicTacToe from "./Tic-Tac-Toe";
-import Clock1 from "./clock";
-import Counter from "./counter1";
+
 import AllInOneTools from "./AllInOneTools.jsx";
 
 
